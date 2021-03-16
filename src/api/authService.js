87fd386bebbbbb9196d2ac70e05db6f59e49e11c
Mladen Terzic <terza98@hotmail.rs>
2021-03-16 11:52:01 +0100

    add api calls to get all employees and update employee state

---
+++ src/api/authService.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API_URL = 'https://60508361534609001766feb9.mockapi.io/';
+
+export const getAllEmployees = () => {
+  return axios.get(API_URL + 'employees');
+};
+
+export const updateEmployeeState = (id, stateName) => {
+  return axios.put(API_URL + `employees/${id}`, { state: stateName });
+};
